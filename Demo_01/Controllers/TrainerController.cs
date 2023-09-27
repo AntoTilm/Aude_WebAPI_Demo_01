24f365e9b97ba11a3a30a215bc3da48c44170746
Aude Beurive <aude.beurive@bstorm.be>
2023-09-27 15:08:46 +0200

    on a fait le getAll youpi et on a fait l'injection de dépendances youpibis

---
+++ Demo_01/Controllers/TrainerController.cs
@@ -0,0 +1,31 @@
+﻿using Demo_01.DAL.Entities;
+using Demo_01.DAL.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Demo_01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrainerController : ControllerBase
+    {
+        private TrainerRepository _TrainerRepository;
+        public TrainerController(TrainerRepository trainerRepository) 
+        { 
+            _TrainerRepository = trainerRepository;
+            // 1 (mauvaise) façon d'injecter notre TrainerRepository
+            //_TrainerRepository = new TrainerRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetTrainers()
+        {
+            IEnumerable<Trainer> trainers = _TrainerRepository.GetAll();
+            return Ok(trainers);
+            //return Ok(new { message = "Hello les .Net de Technobel" });
+            //OU
+            //return StatusCode(StatusCodes.Status200OK, "Hello les .Net de Technobel");
+        }
+    }
+}
--- Demo_01/Program.cs
+++ Demo_01/Program.cs
@@ -1,6 +1,15 @@
+using Demo_01.DAL.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//builder.Services.AddSingleton<TrainerRepository>();
+// Une seule instance est créée pour toute l'API
+builder.Services.AddScoped<TrainerRepository>();
+// Une instance par demande
+//builder.Services.AddTransient<TrainerRepository>();
+// Une instance par demande, par tâches à faire pour la demande
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
